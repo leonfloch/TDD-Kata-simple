c8b3e2973a0cfcb289175f9e8841b3dbda8110c8
Leonardo Valbuena <leov.calderon@gmail.com>
2018-09-30 19:20:41 -0500

    numero elementos, 1 numero (rojo)

--- SecuenciaNumTest.py
+++ SecuenciaNumTest.py
@@ -6,3 +6,8 @@ class SecuenciaNumTest(TestCase):
     def test_elementos_vacio(self):
         result = SecuenciaNum().procesar_secuencia("")
         self.assertEqual(result[0], 0, "numero de elementos cadena vacia")
+
+
+    def test_elementos_1_numero(self):
+        result = SecuenciaNum().procesar_secuencia("1")
+        self.assertEqual(result[0], 1, "numero de elementos 1 numero")
