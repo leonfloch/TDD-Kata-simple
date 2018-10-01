9bb9ce36c446859c4a9d925455450e4331515958
Leonardo Valbuena <leov.calderon@gmail.com>
2018-09-30 19:37:31 -0500

    arreglo numero de elementos y minimo (verde)

--- SecuenciaNum.py
+++ SecuenciaNum.py
@@ -4,12 +4,14 @@ class SecuenciaNum:
     def procesar_secuencia(self, secuencia):
 
         numero_elementos = 0
+        minimo = 0
 
         if len(secuencia) > 0:
             arraySecuencia = list(map(int, secuencia.split(",")))
             numero_elementos = len(arraySecuencia)
+            minimo = min(arraySecuencia)
 
-        return [numero_elementos]
+        return [numero_elementos, minimo]
 
 
 
