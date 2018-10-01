4945a5a5f6b93f6162b0ca85296038a158d34a51
Leonardo Valbuena <leov.calderon@gmail.com>
2018-09-30 19:24:16 -0500

    numero elementos, 1 numero (verde)

--- SecuenciaNum.py
+++ SecuenciaNum.py
@@ -4,6 +4,10 @@ class SecuenciaNum:
     def procesar_secuencia(self, secuencia):
         numero_elementos = 0
 
+        if len(secuencia) > 0:
+            numero_elementos = 1
+
+
         return [numero_elementos]
 
 
