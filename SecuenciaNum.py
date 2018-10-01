6f8b1aeb437c5b049912c9911074e378360865b1
Leonardo Valbuena <leov.calderon@gmail.com>
2018-09-30 19:50:10 -0500

    arreglo: numero elementos, minimo, maximo, promedio (verde)

--- SecuenciaNum.py
+++ SecuenciaNum.py
@@ -6,14 +6,17 @@ class SecuenciaNum:
         numero_elementos = 0
         minimo = 0
         maximo = 0
+        promedio = 0
 
         if len(secuencia) > 0:
             arraySecuencia = list(map(int, secuencia.split(",")))
             numero_elementos = len(arraySecuencia)
             minimo = min(arraySecuencia)
             maximo = max(arraySecuencia)
+            suma = sum(arraySecuencia)
+            promedio = promedio = suma / numero_elementos
 
-        return [numero_elementos, minimo, maximo]
+        return [numero_elementos, minimo, maximo, promedio]
 
 
 
